d0bff5bc6e9804514551c74de41637c70a59b912
pfan <gemini0525@foxmail.com>
2023-12-05 20:58:26 +0800

    Add new routes and components for banner, album, about, training, and news pages

--- src/router/modules/home.js
+++ src/router/modules/home.js
@@ -1,5 +1,10 @@
 const Layout = () => import('@/layout/index.vue')
 const Home = () => import('@/views/home/index.vue')
+const Banner = () => import('@/views/Banner/index.vue')
+const Album = () => import('@/views/Album/index.vue')
+const About = () => import('@/views/About/index.vue')
+const Training = () => import('@/views/Training/index.vue')
+const News = () => import('@/views/News/index.vue')
 
 export default [
   {
@@ -13,10 +18,55 @@ export default [
     children: [
       {
         path: '',
-        name: 'home',
+        name: 'Home',
         component: Home,
         meta: {
-          title: 'Homepage',
+          title: '首页',
+          affix: true,
+        },
+      },
+      {
+        path: '/banner',
+        name: 'banner',
+        component: Banner,
+        meta: {
+          title: 'Banner',
+          affix: true,
+        },
+      },
+      {
+        path: '/album',
+        name: 'album',
+        component: Album,
+        meta: {
+          title: '相册',
+          affix: true,
+        },
+      },
+      {
+        path: '/about-us',
+        name: 'about us',
+        component: About,
+        meta: {
+          title: '关于我们',
+          affix: true,
+        },
+      },
+      {
+        path: '/training-course',
+        name: 'Training course',
+        component: Training,
+        meta: {
+          title: '培训课程',
+          affix: true,
+        },
+      },
+      {
+        path: '/news',
+        name: 'News',
+        component: News,
+        meta: {
+          title: '新闻资讯',
           affix: true,
         },
       },
---
+++ src/views/training/index.vue
@@ -0,0 +1,1 @@
+<template>Training</template>
