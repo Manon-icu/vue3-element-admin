fd32906f51362cc397c14934a9c2dd148297addb
honlyHuang <3080820+honlyHuang@users.noreply.github.com>
2025-01-21 00:15:02 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -14,7 +14,7 @@ service.interceptors.request.use(
   config => {
     const { authorization } = useApp()
     if (authorization) {
-      config.headers['WWW-Authorization'] = `Basic ${authorization.auth_key}`
+      config.headers['WWW-Authorization'] = `Bearer ${authorization.auth_key}`
       // config.headers.authorization = 'Bearer ' + authorization
     }
     return config
